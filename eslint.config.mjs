5accd3c4b8fee8c4507886c09541f63dc0b828bd
Vedant Kesarwani <relativityeismc2@gmail.com>
2025-05-21 15:02:35 +0530

    fix: update ESLint rules for TypeScript and React

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,10 +13,12 @@ const eslintConfig = [
     ...compat.extends('next/core-web-vitals', 'next/typescript'),
     {
         rules: {
-            '@typescript-eslint/no-explicit-any': 'off'
+            '@typescript-eslint/no-explicit-any': 'off',
+            'react/no-unescaped-entities': 'off',
+            '@typescript-eslint/no-unused-vars': 'off',
+            'prefer-const': 'off'
         }
     }
 ]
-
 
 export default eslintConfig
